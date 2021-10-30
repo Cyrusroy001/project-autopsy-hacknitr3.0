4b0a0a5493ce490ab686b9f46bc474ae4f5d9b8c
cyrusmew <51240567+Cyrusroy001@users.noreply.github.com>
2021-10-31 00:06:47 +0545

    commit to checkout

---
+++ lib/screens/pdf_page.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
+import 'package:project_autopsy/model/firebase_file.dart';
+import 'package:project_autopsy/screens/doctor/api/firebase_api.dart';
+
+class PdfPage extends StatefulWidget {
+  final FirebaseFile file;
+  final bool fromDoctor;
+
+  const PdfPage({
+    Key key,
+    @required this.file,
+    @required this.fromDoctor
+  }) : super(key: key);
+
+  @override
+  State<PdfPage> createState() => _PdfPageState();
+}
+
+class _PdfPageState extends State<PdfPage> {
+  String urlPDFPath = "";
+  bool exists = true;
+  int _totalPages = 0;
+  int _currentPage = 0;
+  bool pdfReady = false;
+  PDFViewController _pdfViewController;
+  bool loaded = false;
+
+  Future<File> getFileFromUrl(String url, {name}) async {
+    var fileName = widget.file.name;
+    if (name != null) {
+      fileName = name;
+    }
+    try {
+      var data = await http.get(url);
+      var bytes = data.bodyBytes;
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/" + fileName + ".pdf");
+      print(dir.path);
+      File urlFile = await file.writeAsBytes(bytes);
+      return urlFile;
+    } catch (e) {
+      throw Exception("Error opening url file");
+    }
+  }
+
+  void requestPermission() async {
+    await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+  }
+
+  @override
+  void initState() {
+    requestPermission();
+    getFileFromUrl(widget.file.url).then(
+      (value) => {
+        setState(() {
+          if (value != null) {
+            urlPDFPath = value.path;
+            loaded = true;
+            exists = true;
+          } else {
+            exists = false;
+          }
+        })
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(urlPDFPath);
+    if (loaded) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('PDF Viewer'),
+          foregroundColor: Colors.black87,
+          backgroundColor: Theme.of(context).primaryColor,
+          actions: [
+            IconButton(
+              onPressed: () async {
+                await FirebaseApi.downloadFile(widget.file.ref);
+
+                final snackBar = SnackBar(
+                  content: Text('Downloaded ${widget.file.name}'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              icon: Icon(
+                Icons.file_download,
+              ),
+            ),
+            IconButton(
+              onPressed: () async {
+                
+                await FirebaseApi.deleteFile(widget.file.ref);
+                setState(() {});
+
+                final snackBar = SnackBar(
+                  content: Text('Deleted ${widget.file.name}'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              icon: Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+        body: PDFView(
+          filePath: urlPDFPath,
+          autoSpacing: true,
+          enableSwipe: true,
+          pageSnap: true,
+          swipeHorizontal: true,
+          nightMode: false,
+          onError: (e) {
+            //Show some error message or UI
+          },
+          onRender: (_pages) {
+            setState(() {
+              _totalPages = _pages;
+              pdfReady = true;
+            });
+          },
+          onViewCreated: (PDFViewController vc) {
+            setState(() {
+              _pdfViewController = vc;
+            });
+          },
+          onPageChanged: (int page, int total) {
+            setState(() {
+              _currentPage = page;
+            });
+          },
+          onPageError: (page, e) {},
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.chevron_left),
+              iconSize: 50,
+              color: Colors.black,
+              onPressed: () {
+                setState(() {
+                  if (_currentPage > 0) {
+                    _currentPage--;
+                    _pdfViewController.setPage(_currentPage);
+                  }
+                });
+              },
+            ),
+            Text(
+              "${_currentPage + 1}/$_totalPages",
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            IconButton(
+              icon: Icon(Icons.chevron_right),
+              iconSize: 50,
+              color: Colors.black,
+              onPressed: () {
+                setState(() {
+                  if (_currentPage < _totalPages - 1) {
+                    _currentPage++;
+                    _pdfViewController.setPage(_currentPage);
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      );
+    } else {
+      if (exists) {
+        //Replace with your loading UI
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Text(""),
+          ),
+          body: Text(
+            "",
+            style: TextStyle(fontSize: 20),
+          ),
+        );
+      } else {
+        //Replace Error UI
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Error!"),
+            foregroundColor: Colors.black87,
+          ),
+          body: Center(
+            child: Text(
+              "PDF Not Available",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        );
+      }
+    }
+  }
+}
