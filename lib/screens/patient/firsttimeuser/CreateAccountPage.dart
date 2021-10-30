f4abff1b0205ef12423f668361b80421a399cce0
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2021-10-30 23:31:34 +0530

    Asutosh Final Push

--- lib/screens/patient/firsttimeuser/CreateAccountPage.dart
+++ lib/screens/patient/firsttimeuser/CreateAccountPage.dart
@@ -20,27 +20,76 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
         foregroundColor: Colors.black87,
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/FinishingPage');
-          },
-          child: Text(
-            'Save and Move to Step 2',
-            style: TextStyle(
-              color: Color(0xFF1D1D1D),
-              fontSize: 20,
-              letterSpacing: 2,
-              wordSpacing: 3,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: buildID(),
             ),
-          ),
-          color: Theme.of(context).accentColor,
-          shape: RoundedRectangleBorder(
-            side: BorderSide(
-              color: Colors.black87,
+            SizedBox(
+              height: 5,
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: buildPassword(),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: buildUser(),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/FinishingPage');
+              },
+              child: Text(
+                'Save and Move to Step 2',
+                style: TextStyle(
+                  color: Color(0xFF1D1D1D),
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  wordSpacing: 3,
+                ),
+              ),
+              color: Theme.of(context).accentColor,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Widget buildID() => TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          prefixIcon: Icon(Icons.account_circle),
+          labelText: 'Username',
+        ),
+      );
+
+  Widget buildPassword() => TextField(
+    decoration: InputDecoration(
+      border: OutlineInputBorder(),
+      prefixIcon: Icon(Icons.account_circle),
+      labelText: 'Email',
+    ),
+  );
+
+  Widget buildUser() => TextField(
+    decoration: InputDecoration(
+      border: OutlineInputBorder(),
+      prefixIcon: Icon(Icons.account_circle),
+      labelText: 'Password',
+    ),
+  );
 }
